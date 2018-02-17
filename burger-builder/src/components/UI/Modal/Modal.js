ea527d5667289e80c07027d691241f406db646e3
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-17 09:01:08 -0600

    update

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import classes from './Modal.css';
 
-const modal = (props) => {
+const modal = (props) => (
   <div className={classes.Modal}>
     {props.children}
   </div>
-}
+);
 
 export default modal;
