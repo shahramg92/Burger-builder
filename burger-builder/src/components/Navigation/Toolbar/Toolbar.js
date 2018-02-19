5aae1625b15f8f77743fae4774b55b9e2e58caf7
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-19 17:07:58 -0600

    update

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+
 const toolbar = (props) => (
-  <header>
+  <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>
       <ul>...</ul>
     </nav>
