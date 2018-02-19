3d26dd6e1f5ca1ac9dbc344aaf93cd3f3493400d
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-19 15:30:17 -0600

    update

---
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const toolbar = (props) => (
+  <header>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>
+      <ul>...</ul>
+    </nav>
+  </header>
+);
+
+export default toolbar;
