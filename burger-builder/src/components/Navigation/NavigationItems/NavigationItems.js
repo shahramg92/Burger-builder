b244a09a134ec89dc781efc072eeda732bb2b247
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-19 18:06:59 -0600

    update

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const navigationItems = () => (
-  <ul>
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
 );
 
