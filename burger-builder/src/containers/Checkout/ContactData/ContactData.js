ae5151fa3f7da597bd8f95ec9ce398510836b613
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-24 13:09:02 -0500

    update

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
   state = {
@@ -10,7 +11,35 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    // console.log(this.props.ingredients);
+    this.setState( { loading: true } );
+    const order = {
+        ingredients: this.props.ingredients,
+        price: this.props.totalPrice,
+        customer: {
+            name: 'Max Schwarzmüller',
+            address: {
+                street: 'Teststreet 1',
+                zipCode: '41351',
+                country: 'Germany'
+            },
+            email: 'test@test.com'
+        },
+        deliveryMethod: 'fastest'
     }
+    axios.post( '/orders.json', order )
+        .then( response => {
+            this.setState( { loading: false } );
+        } )
+        .catch( error => {
+            this.setState( { loading: false } );
+        } );
   }
 
   render () {
@@ -22,7 +51,7 @@ class ContactData extends Component {
           <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
           <input className={classes.Input} type="text" name="street" placeholder="Street" />
           <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success">ORDER</Button>
+          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
       </div>
     );
