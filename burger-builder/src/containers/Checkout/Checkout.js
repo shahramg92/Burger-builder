63b43e395526bd91927f257770f3d76b68d8621f
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-24 18:41:37 -0500

    update

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -41,7 +41,9 @@ checkoutContinuedHandler = () => {
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
           />
-        <Route path={this.props.match.path + '/contact-data'} render={() => (<ContactData ingredients={this.state.ingredients}/>)} price={this.state.totalPrice} />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
       </div>
     )
   }
