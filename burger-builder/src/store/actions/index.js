6956565009d42ad31fb673d7658f4e69abfa5573
Shahram Ghassemi <shahramg92@hotmail.com>
2018-04-04 18:52:44 -0500

    update

--- burger-builder/src/store/actions/index.js
+++ burger-builder/src/store/actions/index.js
@@ -1,6 +1,8 @@
 export {
   addIngredient,
   removeIngredient,
-  initIngredients}
-  from './burgerBuilder';
-export {} from './order';
+  initIngredients
+} from './burgerBuilder';
+export {
+  purchaseBurger
+} from './order';
