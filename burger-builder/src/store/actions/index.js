d8a753a53f7ecfdb595aa070ce80934a720fd4d5
Shahram Ghassemi <shahramg92@hotmail.com>
2018-04-30 18:07:14 -0500

    update sagas/actions

--- burger-builder/src/store/actions/index.js
+++ burger-builder/src/store/actions/index.js
@@ -12,5 +12,10 @@ export {
     auth,
     logout,
     setAuthRedirectPath,
-    authCheckState
+    authCheckState,
+    logoutSucceed,
+    authStart,
+    authSuccess,
+    authFail,
+    checkAuthTimeout
 } from './auth';
